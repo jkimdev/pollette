caef81f933411666e7a2bf936b69299c5fe07c35
김재민 <gimjaemin@gimjaemins-MacBook-Air.local>
2024-08-10 22:01:49 +0900

    chore: add dummy scene

---
+++ pollette/PlayScene.swift
@@ -0,0 +1,15 @@
+//
+//  PlayScene.swift
+//  pollette
+//
+//  Created by 김재민 on 8/10/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class PlayScene: SKScene {
+    override func sceneDidLoad() {
+        self.scene?.backgroundColor = .blue
+    }
+}
